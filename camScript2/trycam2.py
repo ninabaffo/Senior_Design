eed74d104f1e5079fb5efdd1af83db2a87afdf18
Nina <nbaffo@uiowa.edu>
2024-04-11 20:01:07 -0500

    added print

--- camScript2/trycam2.py
+++ camScript2/trycam2.py
@@ -4,9 +4,9 @@ from picamera2 import Picamera2  # this library is how we grab the frame
 piCam = Picamera2() # create camera object 
 
 # set camera settings 
-piCam.preview_configuration.main.size=(700, 400)
+piCam.preview_configuration.main.size=(640, 360)
 piCam.preview_configuration.main.format = "RGB888"
-piCam.preview_configuration.controls.FrameRate=1
+piCam.preview_configuration.controls.FrameRate=30
 piCam.preview_configuration.align()
 piCam.configure("preview")
 piCam.start()
@@ -15,6 +15,9 @@ while True:
     # measure framerate 
     tStart=time.time()
     frame=piCam.capture_array() # grab the frame 
+    
+    # print frames per second on video 
+    cv2.putText(frame,str(int(fps)), (30,60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 3)
     cv2.imshow("piCam", frame)  # display the frame
 
     if cv2.waitKey(1)==ord('q'):    # press 'q' to exit out of the camera preview
@@ -22,5 +25,6 @@ while True:
     tEnd=time.time()
     loopTime=tStart-tEnd 
     fps=1/loopTime 
-    print(fps)
+    #print(fps)
+
 cv2.destroyAllWindows()
